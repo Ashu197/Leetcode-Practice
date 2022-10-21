137ccbb668446c0e25df542b7b4b3e57b0a2a99b
Ashutosh Jha <65724647+Ashu197@users.noreply.github.com>
2022-10-21 23:56:52 +0530

    Time: 184 ms (95.03%), Space: 77.1 MB (74.44%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,48 +1,23 @@
 class Solution {
 public:
-    vector<int> nextSmallerElementIndex(vector<int>& heights,int n){
-        stack<int> st;
-        vector<int> res(n);
-        st.push(-1);
-        for(int i=n-1;i>=0;i--){
-            int curr=heights[i];
-            while(st.top()!=-1 && heights[st.top()]>=curr){
-                st.pop();
-            }
-            res[i]=st.top();
-            st.push(i);
-        }
-        return res;
-    }
-    
-    vector<int> prevSmallerElementIndex(vector<int>& heights,int n){
-        stack<int> st;
-        vector<int> res(n);
-        st.push(-1);
-        for(int i=0;i<n;i++){
-            int curr=heights[i];
-            while(st.top()!=-1 && heights[st.top()]>=curr){
-                st.pop();
-            }
-            res[i]=st.top();
-            st.push(i);
-        }
-        return res;
-    }
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        //Storing next smaller element index
-        vector<int> next=nextSmallerElementIndex(heights,n);
-        //Storing prev smaller element index
-        vector<int> prev=prevSmallerElementIndex(heights,n);
+        int ans=INT_MIN, area_with_top=0, top=0, i=0, n=heights.size();
+        stack<int> s;
         
-        int ans=INT_MIN;
-        for(int i=0;i<n;i++){
-            int l=heights[i];
-            if(next[i]==-1) next[i]=n;
-            int b=next[i]-prev[i]-1;
-            ans=max(ans,l*b);
+        while(i<n){
+            if(s.empty() || heights[s.top()]<=heights[i]) s.push(i++);
+            else{
+                top = s.top(); s.pop();
+                area_with_top = heights[top]*(s.empty() ? i : i-s.top()-1);
+                ans = max(ans, area_with_top);
+            }
         }
+        
+        while(!s.empty()){
+            top = s.top(); s.pop();
+            area_with_top = heights[top]*(s.empty() ? i : i-s.top()-1);
+            ans = max(ans, area_with_top);
+        }  
         return ans;
     }
 };
